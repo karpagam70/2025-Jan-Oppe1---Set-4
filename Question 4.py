f6e3f75af7abb3fedf890fd2c0adf74bcecb2ccf
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:56:28 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,16 @@
+'''
+First Non-Repeating Character in a String
+Write a function first_non_repeating_char(s) that returns the first non-repeating character in the string s. If no such character exists, return None.
+
+Example
+
+>>> s = "abcab" # c is the only non repeating char
+>>> first_non_repeating_char(s)
+'c'
+>>> s = "aabbcc"
+>>> first_non_repeating_char(s) # all chars are repeating
+None
+>>> s = "buzzingbee" # u, i, n and g are non repeating and first is "u"
+>>> first_non_repeating_char(s)
+'u'
+'''
