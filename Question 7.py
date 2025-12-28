26beece022094a05b09cc03567915eadb3f81933
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:58:52 +0530

    Create Question 7.py

---
+++ Question 7.py
@@ -0,0 +1,37 @@
+'''
+Pattern Printing - Diamond
+Given an odd number n as an input print a diamond pattern with '*'' in n lines.
+
+Examples
+
+Input
+
+3
+Output
+
+ *
+***
+ *
+Input
+
+5
+Output
+
+  *
+ ***
+*****
+ ***
+  *
+Input
+
+7
+Output
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+'''
